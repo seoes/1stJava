a38cdff13b0ecf335a8346453cb7c343cd92c462
kny2383 <kny2383@gmail.com>
2022-11-30 19:33:02 +0900

    make nayeong branch & Daily/WeeklyBoxOffice

--- Server/src/main/java/movieweb/webapp/service/boxoffice/DailyBoxOfficeApi.java
+++ Server/src/main/java/movieweb/webapp/service/boxoffice/DailyBoxOfficeApi.java
@@ -1,23 +1,27 @@
 package movieweb.webapp.service.boxoffice;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
+        import com.fasterxml.jackson.core.JsonProcessingException;
+        import com.fasterxml.jackson.databind.ObjectMapper;
+        import org.springframework.http.HttpEntity;
+        import org.springframework.http.HttpHeaders;
+        import org.springframework.http.HttpMethod;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
+        import org.springframework.web.client.HttpClientErrorException;
+        import org.springframework.web.client.HttpServerErrorException;
+        import org.springframework.web.client.RestTemplate;
+        import org.springframework.web.util.UriComponents;
+        import org.springframework.web.util.UriComponentsBuilder;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
+        import java.text.SimpleDateFormat;
+        import java.util.Calendar;
+        import java.util.Date;
+        import java.util.HashMap;
+        import java.util.Map;
 
 @RestController
-public class DailyBoxOfficeApi {
+public class DailyBoxOfficeApi{
 
     @GetMapping("movie")
     public String getAPI() {
@@ -26,6 +30,7 @@ public class DailyBoxOfficeApi {
         String jsonInString = "";
 
         try {
+
             RestTemplate restTemplate = new RestTemplate();
 
             HttpHeaders header = new HttpHeaders();
@@ -36,8 +41,8 @@ public class DailyBoxOfficeApi {
             cal.setTime(new Date());
             cal.add(Calendar.DATE, -1);
 
-
             UriComponents uri = UriComponentsBuilder.fromHttpUrl(url+"?"+"key=a8285e93378ce5754e74c4b3c7f42aa8&targetDt="+DATE_FMT.format(cal.getTime())).build();
+
 
             //이 한줄의 코드로 API를 호출해 MAP타입으로 전달 받는다.
             ResponseEntity<Map> resultMap = restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Map.class);
@@ -45,14 +50,9 @@ public class DailyBoxOfficeApi {
             result.put("header", resultMap.getHeaders()); //헤더 정보 확인
             result.put("body", resultMap.getBody()); //실제 데이터 정보 확인
 
-            LinkedHashMap lm = (LinkedHashMap)resultMap.getBody().get("boxOfficeResult");
-            ArrayList<Map> dboxoffList = (ArrayList<Map>)lm.get("dailyBoxOfficeList");
-            LinkedHashMap mnList = new LinkedHashMap<>();
-            for (Map obj : dboxoffList) {
-                mnList.put(obj.get("rnum"),obj.get("movieNm"));
-            }
+            //데이터를 제대로 전달 받았는지 확인 string형태로 파싱해줌
             ObjectMapper mapper = new ObjectMapper();
-            jsonInString = mapper.writeValueAsString(mnList);
+            jsonInString = mapper.writeValueAsString(resultMap.getBody());
 
         } catch (HttpClientErrorException | HttpServerErrorException e) {
             result.put("statusCode", e.getRawStatusCode());
