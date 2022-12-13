2412d5d83b8860a265dfbb399f144169b339c6e0
TheLifeOfSeo <dsa3182@gmail.com>
2022-12-14 01:01:34 +0900

    fix register

--- Server/src/Svelte/src/Component/User/register.svelte
+++ Server/src/Svelte/src/Component/User/register.svelte
@@ -27,101 +27,6 @@
                 location.href="/";
             })
     }
-<<<<<<< HEAD
-    </script>
-    
-    
-    
-    <div id="app">
-        </div>
-        <div id = header_title>
-            WMM Login
-          </div>
-        <table>
-            <tr>
-                <td>ID</td>
-                <td>
-                    <input type="text" id = "userID" name="userID"  bind:value={userID} style= "width:270px; height:23px;" />
-                </td>
-            <tr>
-                <td>NAME</td>
-                <td>
-                    <input type="text" id="userName" name="userName"  bind:value={userName} style= "width:270px; height:23px;" />
-                </td>
-            </tr>
-            <tr>
-                <td>EMAIL</td>
-                <td>
-                    <input type="text" id="userEmail" name="userEmail" bind:value={userEmail} style= "width:270px; height:23px;" />
-                </td>
-            </tr>
-            <tr>
-                <td>TEL</td>
-                <td>
-                    <input type="text" id="userTel" name="userTel"  bind:value={userTel} style= "width:270px; height:23px;" />
-                </td>
-            </tr>
-            <tr>
-                <td>PASSWORD</td>
-                <td>
-                    <input type="text" id="password" name="password"  bind:value={password} style= "width:270px; height:23px;" />
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <button type="button"class="uk-button uk-button-default" on:click={submitInfo} id="account">회원가입</button>
-                </td>
-            </tr>
-        </table>
-    
-    
-    <style>
-    #app {
-    position: absolute;
-    margin: 0 auto;
-    width: 500px;
-    height: 600px;
-    left: 692px;
-    top: 250px;
-    background: #B9B9B9;
-    opacity: 0.2;
-    border-radius: 100px;
-    z-index:0;
-      }
-    
-    #header_title{
-    position: relative;
-    width: 530px;
-    height: 200px;
-    left: 800px;
-    top: 200px;
-    
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 100;
-    font-size: 30px;
-    line-height: 30px;
-    z-index: 2;
-    color: #000000;
-    }
-    
-    table {
-    position: relative;
-    width: 550px;
-    height: 200px;
-    left: 570px;
-    top: 150px;
-    }
-    
-    td {
-          padding: 3 3px;
-    }
-    </style>
-    
-    
-    
-    
-=======
 </script>
 
 
@@ -225,4 +130,3 @@ table, td {
 
 
 
->>>>>>> origin/nayoung
